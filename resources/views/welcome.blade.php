b2df06fa1e17fc076b50cb4e9fc1be25b7a1d8f4
Drew0004 <drdrewgraphic@gmail.com>
2024-02-26 15:33:41 +0100

    Completato esercizio di base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,25 @@
 
 @section('main-content')
 
-@dd ($movies)
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $singleMovie)
+            <div class="col-2">
+                <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Titolo: {{$singleMovie['title']}}</h5>
+                      <h6>Titolo originale: {{$singleMovie['original_title']}}</h6>
+                      <ul class="list-unstyled">
+                        <li>Nazionalità: {{$singleMovie['nationality']}}</li>
+                        <li>Data: {{$singleMovie['date']}}</li>
+                        <li>Voto: {{$singleMovie['vote']}}</li>
+                      </ul>
+                      <a href="#" class="btn btn-primary">Info film</a>
+                    </div>
+                  </div>
+            </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
